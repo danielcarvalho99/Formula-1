4b4a381217b22f728ece5ef5ccfc8da0637d80c0
danielcarvalho99 <danielcd3099@gmail.com>
2026-06-07 21:30:09 -0300

    v1 working

--- extractor.py
+++ extractor.py
@@ -1,34 +1,52 @@
 import fastf1
+import time
+import pandas as pd
 
 class Extractor:
-    def __init__(self, years=list, modes=list):
-        self.years = years
+    def __init__(self, start: int, end: int, modes=list):
+        self.start = start
+        self.end = end
         self.modes = modes
         self.limit = 100
+        self.sleep_time = 5
 
-    def extract_results(self, year: int, gp: int, mode: str):
-        session = fastf1.get_session(year, gp, mode)
-        session._load_drivers_results()
-        return session.results
-    
-    def save_data(self, df, year: int, gp: int, mode: str):
+    def _create_dataframe(self, session: fastf1.core.Session, year: int, gp: int, mode: str):
+        df = session.results
+        events = session.event
+
         df['Year'] = year
         df['GP'] = gp
         df['Mode'] = mode
-        df.to_csv(f"data/{year}_{gp:02}_{mode}.csv", index=False)
-        
-        return None
 
-    def process_data(self, year: int, gp: int, mode: str) -> bool:
-        df = self.extract_results(year, gp, mode)
-        self.save_data(df, year, gp, mode)
-        return True
+        df['Country']  = events['Country']
+        df['Location'] = events['Location']
+        df['OfficialEventName'] = events['OfficialEventName']
+        df['EventDate'] = events['EventDate']
+        df['EventName'] = events['EventName']
+        df['EventFormat'] = events['EventFormat']
+
+        return df
     
-    def process_year_mode(self, year: int, mode: str):
-        for i in range(1, self.limit):
-            try:
-                self.process_data(year, i, mode)
-            except Exception as e:
-                break
-        
-        return None
+    def _process_session(self, year: int, gp: int, mode: str) -> bool:
+        session = fastf1.get_session(year, gp, mode)
+        session._load_drivers_results()
+        df = self._create_dataframe(session, year, gp, mode)
+
+        return df
+    
+    def _save_dataframe(self, df: pd.DataFrame, year: int, gp: int, mode: str):
+        filename = f"{year}_{gp:02}_{mode}.csv"
+        df.to_csv(f"data/{filename}", index=False)
+    
+    def process_data(self):
+        for year in range(self.start, self.end + 1):
+            for mode in self.modes:
+                for gp in range(1, self.limit):
+                    try:
+                        df = self._process_session(year, gp, mode)
+                        self._save_dataframe(df, year, gp, mode)
+                        time.sleep(0.45)
+                    except Exception as e:
+                        print(f"Error occurred while processing {year}_{gp:02}_{mode}: {e}")
+                        break
+            time.sleep(self.sleep_time)
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from client import GCPClient
+from extractor import Extractor
+from writer import Writer
+
 def main():
-    print("Hello from formula-1!")
+
+    # Initialize GCP client and authenticate
+    client = GCPClient("bucket-access.json")
+    client = client.authenticate()
+    
+    start = input("Add start date: ")
+    end = input("Add end date: ")
+    # Extract data and save to local CSV files
+    extractor = Extractor(start=start, end=end, modes=['R'])
+    extractor.process_data()
+
+    # Write local CSV files to GCP bucket
+    writer = Writer(client)
+    files = writer.list_files(path="data/", extension=".csv")
+
+    for file in files:
+        writer.write_to_bucket(local_file_path=f"data/{file}", gcp_file_path=f"bronze/races/{file}", bucket_name="bucket-f1-data")
+        print(f"Wrote {file} to GCP")
 
 
 if __name__ == "__main__":
